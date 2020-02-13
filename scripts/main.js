eafa84f1c4b09b5591cdf40c7f288bac54df788b
Henry Lai <lai-henry@outlook.com>
2020-02-13 14:31:10 +0100

    adding interactivity on our page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+
+/*making the picture change with if function*/
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -7,4 +9,31 @@ myImage.onclick = function() {
     } else {
         myImage.setAttribute ('src','images/overheadpress.png');
     }
+}
+
+/*creating a personalized greeting */
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName () {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'Ready for same gains, ' + myName;
+    }
+}
+
+/* adding if..else block, which is the initialization code */
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Ready for some gains, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName ();
 }
